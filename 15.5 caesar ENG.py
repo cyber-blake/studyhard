d955169c99e0725586eb7252e4623a795700c668
dmitry <self@self.com>
2025-09-02 12:55:40 +0300

    промежуточный рез-т, рефакторинг кода и логики, символы добавлятся в слово, слово апендится в список

--- 15.5 caesar ENG.py
+++ 15.5 caesar ENG.py
@@ -1,21 +1,18 @@
 # 15.5 шифр цезаря
 import string
 s = 'Day, mice. "Year" is a mistake!'
-# shift of Day = 3 (no punct)
-# сделать циклический сдвиг
 # s = input()
-result = ''
-shift = 0
+result = []
+words = s.split() # разбили по пробелу на слова с знаками препинания
 
-
-s_new = s.split()
-
-for word in s_new:
-    shift = word.count(string.ascii_letters)
+for word in words:
+    shift = sum(1 for char in word if char.isalpha())
+    new_word = ''
     for symb in word:
         if symb in string.ascii_letters:
-            result += chr(ord(symb)+shift)
-        else:
-            result += symb
+            new_symb = chr(ord(symb)+shift) # доделать через джоин
+            new_word += new_symb
+        result.append(new_word)
+# сделать циклический сдвиг
 
 print(result)
