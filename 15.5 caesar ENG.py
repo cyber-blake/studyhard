f7aebeaabaa5e17b134306856246364158dd4425
dmitry <self@self.com>
2025-09-02 13:33:29 +0300

    исправлена ошибка апенда

--- 15.5 caesar ENG.py
+++ 15.5 caesar ENG.py
@@ -1,19 +1,19 @@
-# 15.5 шифр цезаря
+# * 15.5 шифр цезаря
 import string
 s = 'Day, mice. "Year" is a mistake!'
 # s = input()
 result = []
-words = s.split() # разбили по пробелу на слова с знаками препинания
+words = s.split() # ? разбили по пробелу на слова с знаками препинания
 
 for word in words:
-    shift = sum(1 for char in word if char.isalpha())
     new_word = ''
+    shift = sum(1 for char in word if char.isalpha())
     for symb in word:
         if symb in string.ascii_letters:
-            new_symb = chr(ord(symb)+shift) # доделать через джоин
+            new_symb = chr(ord(symb)+shift) # ? доделать через джоин
             # нужно сохранить пунктуацию
             new_word += new_symb
-        result.append(new_word)
-# сделать циклический сдвиг
+    result.append(new_word)
+# ? сделать циклический сдвиг
 
 print(result)
