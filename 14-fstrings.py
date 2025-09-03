5e3211d1c24b6b267721102e5a83766b6f13b682
dmitry <self@self.com>
2025-09-03 17:00:01 +0300

    exercise 14 f-strings

---
+++ 14-fstrings.py
@@ -0,0 +1,36 @@
+# №1
+# names = ["Александр","Алекс","Альберт"]
+# surnames = ["Вотяк","Вотяков","Вотякович"]
+# paternis = ["Романович"]
+
+# for x in names:
+#     for y in surnames:
+#         print(f"Диплом с отличием вручается {y}y {x}y {paternis[0]}у")
+
+# * №2
+# A = input()
+# B,C = int(input()), int(input())
+
+# print(f'{A}{B:04}-{C:03}')
+
+
+# * №3
+# first = int(input())
+# second = int(input())
+# print(f'{first:>9}')
+# print(f'{second:>9}')
+# print(f'{first+second:>9}')
+# ! расскоментить перед сдачей на пастебин
+
+# * №4
+# r = int(input()) / 10  # rate
+# k = int(input())
+# summ = 100_000_000
+# res = summ * ((1 + r / 12) ** k)
+# print(f"{res:,.2f}")
+
+
+# * №5
+
+
+print(f"[DEBUG] {a=} {b=} {result=}")
