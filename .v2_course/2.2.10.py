a806a5ca146f79801820d2468af46b5115f31732
Dmitry <blakethespend@gmail.com>
2025-09-09 09:16:59 +0300

    final final

---
+++ .v2_course/2.2.10.py
@@ -0,0 +1,10 @@
+list = []
+for x in range(int(input())):
+    list.append(input())
+
+anton = ("a", "n", "t", "o", "n")
+for x in list:
+    if all()
+
+num2 = [1, 2.0, 3.1, 4, 5, 6, 7.9]
+all([type(x) is int for x in num2])
--- votyakov/adv_python/16_files.py
+++ votyakov/adv_python/16_files.py
@@ -1,10 +1,11 @@
+#
 # * 1
 def all_file(file):
     with open(file, encoding="utf-8") as f:
         return f.read()
 
 
-# print(all_file("input.txt"))
+print(all_file("input.txt"))
 
 
 # * 2
@@ -13,7 +14,7 @@ def one_line(file):
         return f.readline()
 
 
-# print(one_line("input.txt"))
+print(one_line("input.txt"))
 
 
 # * 3
@@ -23,7 +24,7 @@ def all_lines(file):
         return L1
 
 
-# print(all_lines("input.txt"))
+print(all_lines("input.txt"))
 
 
 # * 4
@@ -34,7 +35,7 @@ def all_lines_stripped(file):
         return l1
 
 
-# print(all_lines_stripped("input.txt"))
+print(all_lines_stripped("input.txt"))
 
 
 # * 5
@@ -44,7 +45,7 @@ def all_lines_printer(file):
             print(line)
 
 
-# all_lines_printer("input.txt")
+all_lines_printer("input.txt")
 
 
 # * 6
@@ -56,7 +57,7 @@ def spaced_lines(file):
         return s
 
 
-# print(spaced_lines("input.txt"))
+print(spaced_lines("input.txt"))
 
 s1 = "Далее копируем весь текст который появляется при нажатии на файл."
 
@@ -78,3 +79,70 @@ def no_punctuation(string):
 
 
 print(no_punctuation(s))
+
+
+# * 9
+def write_string(file, string):
+    with open(file, "a", encoding="utf-8") as f:
+        f = f.write(string)
+
+
+write_string("input.txt", "Hallo Welt!")
+
+
+# * 10
+def write_string_v2(file, string):
+    with open(file, "a", encoding="utf-8") as f:
+        string += "\n"
+        f = f.write(string)
+
+
+write_string_v2("input.txt", "Hola mundo!")
+
+
+# * 11
+def write_lines(file, list1):
+    with open(file, "a", encoding="utf-8") as f:
+        f = f.writelines(list1)
+
+
+l = ["abc", "def", "ghi", "jkl", "mno", "pqrs"]
+write_lines("input.txt", l)
+
+
+# * 12
+def transport(file1, file2):
+    with open(file1, encoding="utf-8") as f1:
+        content = f1.read()
+    with open(file2, "w", encoding="utf-8") as f2:
+        print(content, file=f2)
+
+
+transport("input.txt", "output.txt")
+
+
+# * 13
+def lines_in_between(A, B):
+    """
+    13. функция, которая принимает на вход сначала имя первого файла, затем имя
+    второго файла, а после записывает во второй файл все строки, которые на-
+    чинаются с подстроки <hello>, а заканчиваются на подстроку <world>. Ис-
+    пользуйте методы .startswith() и .endswith()
+    """
+    with open(A, "r", encoding="utf-8") as f:
+        with open(B, "w", encoding="utf-8") as g:
+            for line in f:
+                tempLine = line.strip()
+                if tempLine.startswith("hello") and tempLine.endswith("world"):
+                    g.write(line)
+
+
+# * 14
+def processDictionary(name):
+    dictionary = []
+    with open(name, "r", encoding="utf-8") as f:
+        data = f.readlines()[1:]
+    for line in data:
+        temp = line.split()
+        dictionary[temp[0]] = (temp[1], temp[2])
+    return dictionary
