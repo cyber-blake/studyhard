19722d9dc8191ec93db37fff7e98a6fcfe4ee2f4
Dmitry <blakethespend@gmail.com>
2025-09-17 18:12:46 +0300

    working on it

--- votyakov/OOP/24_classes.py
+++ votyakov/OOP/24_classes.py
@@ -75,20 +75,14 @@ class CoffeeMachine:
 
 
 class PhotoCamera:
-    def __init__(
-        self,
-        brand,
-        model,
-        resolution,
-        memory_capacity,
-    ):
+    def __init__(self, brand, model, resolution=(1920, 1080), memory_capacity=10):
         self.brand = brand
         self.model = model
         self.resolution = resolution
         self.memory_capacity = memory_capacity
-        self.photos = {}
         self.is_on = False
         self.photo_count = 0
+        self.photos = {}
 
     def take_photo(self):
         import time
@@ -97,8 +91,9 @@ class PhotoCamera:
             print(
                 f"Сделана фотография с разрешением {self.resolution[0]}x{self.resolution[1]} под именем Photo{self.photo_count:03d}"
             )
+            self.store_photo()
         else:
-            print("как ты собрался фоткать на выключенную камеру?")
+            print("Как ты собрался фоткать на выключенную камеру?")
 
     def get_camera_info(self):
         return f"Марка: {self.brand}, Модель: {self.model}, Разрешение: {self.resolution[0]}x{self.resolution[1]}"
@@ -115,27 +110,98 @@ class PhotoCamera:
         return self.is_on
 
     def store_photo(self):
-        import time
+        if len(self.photos) < self.memory_capacity:
+            import time
 
-        self.photos[f"Photo{self.photo_count:03d}"] = (
-            (self.resolution[0], self.resolution[1]),
-            time.ctime(),
-        )
-        self.photo_count += 1
+            current_time = time.ctime()
+            photo_name = f"Photo{self.photo_count:03d}"
+            self.photos[photo_name] = (
+                (self.resolution[0], self.resolution[1]),
+                current_time,
+            )
+            print(f"Фото {photo_name} успешно сохранено")
+            self.photo_count += 1  # Увеличиваем после сохранения
+        else:
+            print("Память переполнена. Удалите фотографии")
 
     def view_photos(self):
+        if not self.photos:
+            print("Нет сохраненных фотографий")
+            return
+
+        print("\n=== СОХРАНЕННЫЕ ФОТОГРАФИИ ===")
         for key, value in self.photos.items():
-            print(f"{key}:{value}")
+            print(f"{key}, разрешение: {value[0][0]}x{value[0][1]}, время: {value[1]}")
+        print(f"Всего фотографий: {len(self.photos)}/{self.memory_capacity}")
 
     def clear_memory(self):
         self.photos.clear()
+        self.photo_count = 0
+        print("Фото удалены")
 
 
-# take_photo import pic_screenshot
-canon = PhotoCamera("canon", "077D", (1920, 1080), 100)
-print(canon.is_on)
-print(canon.get_camera_info())
-canon.turn_on()
-canon.take_photo()
-canon.store_photo()
-canon.view_photos()
+# Cam1 = PhotoCamera("Сanon", "077D", (1920, 1080), 2)
+# print(Cam1.get_camera_info())
+# print(Cam1.is_camera_on())
+# Cam1.turn_on()
+# Cam1.take_photo()
+# Cam1.take_photo()
+# Cam1.take_photo()
+# Cam1.store_photo()
+# Cam1.store_photo()
+# Cam1.view_photos()
+# Cam1.clear_memory()
+# Cam1.turn_off()
+
+# ?_____________________________________________________________
+
+
+# * 3
+class Revolver:
+    def __init__(self):
+        from itertools import cycle
+
+        self.MAX_CAPACITY = 6
+        non_tuple = list(range(1, 7))
+        self.slots = dict.fromkeys(non_tuple, None)  # словарь 1: None, 2: None, etc
+        self.slots_cycle = cycle(self.slots)
+        self.slot_pointer = 1
+        print(type(self.slots))
+        print(type(self.slots_cycle))
+        print(type(self.slot_pointer))
+
+    def show_baraban(self):
+        print(self.slots)
+
+    def add_bullet(self, bullet):
+        self.slots[self.slot_pointer] = bullet
+
+    def add_bullets_from_list(self, list):
+        if len(list) <= self.MAX_CAPACITY:
+            ...
+
+    def shoot(self):
+        if self.slot_pointer != None:
+            print("BANG!!!")
+            next(self.slots_cycle)
+        else:
+            return None
+
+    # def unload(self, all_items=False):
+
+    def scroll(self):
+        import random
+
+        self.slot_pointer = random.randint(1, 6)
+        print("Крутите барабан! Буква А! (барабан покручен)")
+        print(self.slot_pointer)
+
+    # def bullet_count(self):
+
+
+# * play russian roulette
+
+
+r1 = Revolver()
+r1.show_baraban()
+r1.scroll()
