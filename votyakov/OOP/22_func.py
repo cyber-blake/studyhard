355e9b28a7291eb6899d9d4836c3a2ce87fc1419
Dmitry <blakethespend@gmail.com>
2025-09-18 15:46:10 +0300

    Finaled function programming

--- votyakov/OOP/22_func.py
+++ votyakov/OOP/22_func.py
@@ -43,65 +43,62 @@ print(reduce_sum(L1))
 # * 6
 def partial_apply(func, x, y):
     def partial_func(func, x):
-        return
+        return partial_func
 
 
 # * 7
-def apply(func, x):  #  ? higher order function
-    return func(x)
+def compose(f, g):
 
+    def h(x):
+        return g(f(x))
 
-def square(n):  # ?  function to be passed
-    return n * n
-
-
-def прибавить_один(x):
-    return x + 1
-
-
-def применить_операцию(список, операция):
-    результат = []
-    for элемент in список:
-        результат.append(операция(элемент))
-    return результат
-
-
-числа = [1, 2, 3]
-новые_числа = применить_операцию(числа, прибавить_один)
-print(новые_числа)  # Выведет: [2, 3, 4]
-
-
-# ? из интернета
-def counter(start=0):  # higher order function
-    count = start
-
-    def increment():  # inner function
-        nonlocal count  # retains access to 'count' even after counter() ends
-        count += 1
-        return count
-
-    return increment  # returns the inner function
-
-
-counter1 = counter(5)  # closure retains count = 5
-print(counter1())
-print(counter1())
-
-counter2 = counter(10)  # new closure with count = 10
-print(counter2())
-
-
-def compose(f, g, arg):
-    h = g(f(arg))
     return h
 
 
-l = [1, 2, 3, 4, 5]
+add_one = lambda x: x + 1
+multiply_by_two = lambda x: x * 2
 
-# g = filter(x % 2 == 0, l)
-f = lambda x: x**2
-print(f(l))
+composed_function = compose(add_one, multiply_by_two)
+
+print(f"Результат композиции: {composed_function(5)}")
 
 
 # * 8
-def create_function_with_arguments(func, *arguments): ...
+def create_function_with_arguments(func, arguments):
+
+    def new_func():
+        return func(*arguments)
+
+    return new_func
+
+
+def sum_numbers(a, b, c):
+    return a + b + c
+
+
+args = [10, 20, 30]
+sum_with_args = create_function_with_arguments(sum_numbers, args)
+
+print(f"Результат: {sum_with_args()}")
+
+
+# * 9
+def compose_functions(functions):
+
+    def composed_function(x):
+        result = x
+        for func in functions:
+            result = func(result)
+        return result
+
+    return composed_function
+
+
+add_five = lambda x: x + 5
+square = lambda x: x**2
+subtract_three = lambda x: x - 3
+
+function_list = [add_five, square, subtract_three]
+final_function = compose_functions(function_list)
+
+print(f"Результат композиции списка функций: {final_function(2)}")
